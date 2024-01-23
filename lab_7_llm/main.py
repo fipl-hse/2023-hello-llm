e08309e90a7883bac73f8a1dbe9ad794e6d595a8
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-01-23 11:11:46 +0300

    dummy update

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -41,6 +41,7 @@ class RawDataImporter(AbstractRawDataImporter):
         Raises:
             TypeError: In case of downloaded dataset is not pd.DataFrame
         """
+        pass
 
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
