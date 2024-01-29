c69f9e00e988192c92d41c2b5cde6fd93ee5934d
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2024-01-29 19:59:39 +0300

    upd

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -4,10 +4,11 @@ Neural machine translation module.
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 from collections import namedtuple
 from pathlib import Path
-from typing import Iterable, Iterator, Sequence
+from typing import Iterable, Sequence
+
 from datasets import load_dataset
-from transformers import BertForSequenceClassification
 from torchinfo import summary
+from transformers import BertForSequenceClassification
 
 try:
     import torch
