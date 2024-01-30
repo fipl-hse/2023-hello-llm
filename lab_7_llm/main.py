e655ebba44665b5ea68c4e2610af4103c6b0eda5
AlekseiBogdan <apbkgdan@gmail.com>
2024-01-30 16:28:28 +0300

    import fixes

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -3,11 +3,12 @@ Neural machine translation module.
 """
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 from collections import namedtuple
-from datasets import load_dataset
 from pathlib import Path
+from typing import Iterable, Sequence
+
+from datasets import load_dataset
 from torchinfo import summary
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from typing import Iterable, Iterator, Sequence
 
 try:
     import torch
