51bef50138d211f681d392f85650051e5b38286e
AlekseiBogdan <apbkgdan@gmail.com>
2024-02-06 21:04:45 +0300

    fix style issues again

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -6,16 +6,15 @@ from collections import namedtuple
 from pathlib import Path
 from typing import Iterable, Sequence
 
+import torch
 import pandas as pd
 from datasets import load_dataset
 from evaluate import load
+from torch import argmax, long, ones
 from torchinfo import summary
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 try:
-    import torch
-    from torch import argmax
-    from torch import ones
     from torch.utils.data import DataLoader
     from torch.utils.data.dataset import Dataset
 
@@ -180,7 +179,7 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             dict: Properties of a model
         """
-        tensor_data = ones(1, self._model.config.max_position_embeddings, dtype=torch.long)
+        tensor_data = ones(1, self._model.config.max_position_embeddings, dtype=long)
         input_data = {'attention_mask': tensor_data,
                       "input_ids": tensor_data}
         analytics = summary(self._model, input_data=input_data, verbose=False)
