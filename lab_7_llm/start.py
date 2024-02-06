2974a78a90e790ad4aaf11a942913d4693f8a574
Anastasia Matuschak <anastasia.mt12@gmail.com>
2024-02-06 17:55:28 +0300

    start py implementation

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -3,7 +3,8 @@ Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
 from core_utils.llm.time_decorator import report_time
-from main import RawDataImporter
+from main import RawDataImporter, RawDataPreprocessor
+import json
 
 @report_time
 def main() -> None:
@@ -12,7 +13,13 @@ def main() -> None:
     """
     dataset = RawDataImporter('self._hf_data')
     dataset.obtain()
+    with open('settings.json') as settings_file:
+        settings = json.load(settings_file)
 
+    importer = RawDataImporter(settings["parameters"]["dataset"])
+    importer.obtain()
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+    result = preprocessor.analyze()
 
 
 if __name__ == "__main__":
