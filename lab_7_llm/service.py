f3e6947e768d1cfc29334ed95657c4b4bd4f7307
AlekseiBogdan <apbkgdan@gmail.com>
2024-02-09 17:30:47 +0300

    update target score, requirements.txt, create server instance and assets files

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -1,10 +1,20 @@
 """
 Web service for model inference.
 """
-# pylint: disable=undefined-variable
-from fastapi import FastAPI
+import json
+import os
+from pathlib import Path
 
-from lab_7_llm.main import LLMPipeline
+# pylint: disable=undefined-variable
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from config.constants import PROJECT_ROOT
+
+from lab_7_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
 def init_application() -> tuple[FastAPI, LLMPipeline]:
@@ -17,6 +27,40 @@ def init_application() -> tuple[FastAPI, LLMPipeline]:
     Returns:
         tuple[fastapi.FastAPI, LLMPipeline]: instance of server and pipeline
     """
+    server = FastAPI()
+
+    with open(PROJECT_ROOT / 'lab_7_llm' / 'settings.json', "r", encoding='utf-8') as settings_path:
+        settings = json.load(settings_path)
+    ds_obtained = RawDataImporter(settings["parameters"]["dataset"])
+    ds_obtained.obtain()
+    preprocessed_ds = RawDataPreprocessor(ds_obtained.raw_data)
+    preprocessed_ds.transform()
+    task_ds = TaskDataset(preprocessed_ds.data.head(100))
+    llm_infer = LLMPipeline(model_name=settings["parameters"]["model"], dataset=task_ds,
+                            max_length=120, batch_size=64, device='cpu')
+
+    return server, llm_infer
 
 
-app, pipeline = None, None
+init_server = init_application()
+
+app, pipeline = init_server[0], init_server[1]
+app_dir = os.path.dirname(__file__)
+assets_abs_file_path = os.path.join(app_dir, "assets")
+print(str(assets_abs_file_path))
+app.mount("/assets", StaticFiles(directory=assets_abs_file_path), name="assets")
+
+BASE_DIR = Path(__file__).resolve().parent
+jinja_template = Jinja2Templates(directory=str(Path(BASE_DIR, 'assets')))
+
+
+@app.get('/')
+def hello(request: Request):
+    return jinja_template.TemplateResponse(
+        name="index.html",
+        request=request
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run("service:app", host='127.0.0.1', port=8000, reload=True)
