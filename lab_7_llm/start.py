4f1fa62ac2ec0a657b21799913023a03d44aeddc
daribbb <bugakovadarad@gmail.com>
2024-02-09 15:15:34 +0300

    some chages again

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -3,15 +3,20 @@ Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
 from core_utils.llm.time_decorator import report_time
-
+from main import RawDataImporter
+import json
+from pathlib import Path
 
 @report_time
 def main() -> None:
     """
     Run the translation pipeline.
     """
-    result = None
-    assert result is not None, "Demo does not work correctly"
+
+    with open(Path('settings.json'), 'r', encoding='utf-8') as settings_file:
+        settings = json.load(settings_file)
+    dataset = RawDataImporter(settings['parameters']['dataset'])
+    dataset.obtain()
 
 
 if __name__ == "__main__":
