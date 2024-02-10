442f20f038e94789ded7e8cf1c1ac826d260e76d
VSvir <VSslava612@yandex.ru>
2024-02-11 02:04:53 +0300

    [feature] initialize service core [chore] add files for service implementation [chore] update requirements.txt

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -2,13 +2,13 @@
 Web service for model inference.
 """
 # pylint: disable=undefined-variable
-try:
-    from fastapi import FastAPI
-except ImportError:
-    print('Library "fastapi" not installed. Failed to import.')
-    FastAPI = None
+import json
 
-from lab_7_llm.main import LLMPipeline
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from config.constants import PROJECT_ROOT
+from lab_7_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
 def init_application() -> tuple[FastAPI, LLMPipeline]:
@@ -21,6 +21,19 @@ def init_application() -> tuple[FastAPI, LLMPipeline]:
     Returns:
         tuple[fastapi.FastAPI, LLMPipeline]: instance of server and pipeline
     """
+    server = FastAPI()
+    server.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+    with open(PROJECT_ROOT / 'lab_7_llm' / 'settings.json', 'r', encoding='utf-8') as settings_file:
+        configs = json.load(settings_file)
+
+    data_loader = RawDataImporter(configs['parameters']['dataset'])
+    data_loader.obtain()
+    preprocessor = RawDataPreprocessor(data_loader.raw_data)
+    preprocessor.transform()
+    dataset = TaskDataset(preprocessor.data.head(100))
+    llm = LLMPipeline(configs['parameters']['model'], dataset, 120, 64, 'cpu')
+    return server, llm
 
 
-app, pipeline = None, None
+app, pipeline = init_application()
