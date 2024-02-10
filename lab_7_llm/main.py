fd7493e9fa1ec6a499bf266e52c0d941a3b5b705
tanynova99 <tanynovark800@gmail.com>
2024-02-10 22:35:16 +0300

    style

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -2,7 +2,6 @@
 Neural machine translation module.
 """
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
-from collections import namedtuple
 from pathlib import Path
 from typing import Iterable, Sequence
 
