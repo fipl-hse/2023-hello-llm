5f61f4f20629f08be27153e2e3199acc6c4d6b2c
Anastasia Matuschak <anastasia.mt12@gmail.com>
2024-02-10 19:17:56 +0300

    fear and loathing in style checks

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -6,8 +6,9 @@ from collections import namedtuple
 from pathlib import Path
 from typing import Iterable, Iterator, Sequence
 
-from torchinfo import summary
 from datasets import load_dataset
+from torchinfo import summary
+
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 try:
