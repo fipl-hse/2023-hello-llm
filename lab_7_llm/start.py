3c22ece4605196c809899fe36a2fe5288bcac5cf
awakenedlilac <90451983+awakenedlilac@users.noreply.github.com>
2024-02-11 10:37:37 +0300

    changes

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -1,12 +1,16 @@
 """
 Neural machine translation starter.
 """
+# pylint: disable= too-many-locals
 import json
 
-# pylint: disable= too-many-locals
 from config.constants import PROJECT_ROOT
 from core_utils.llm.time_decorator import report_time
-from lab_7_llm.main import RawDataImporter, RawDataPreprocessor, LLMPipeline, TaskDataset
+from lab_7_llm.main import (LLMPipeline,
+                            RawDataImporter,
+                            RawDataPreprocessor,
+                            TaskDataset)
+
 
 @report_time
 def main() -> None:
