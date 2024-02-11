d08f02fd49d25cacd20d153050b3d5ee7a74eac6
reginafeles <kate2002elisey@gmail.com>
2024-02-11 14:33:36 +0300

    first

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -5,20 +5,12 @@ Neural machine translation module.
 from collections import namedtuple
 from pathlib import Path
 from typing import Iterable, Iterator, Sequence
-
-try:
-    import torch
-    from torch.utils.data.dataset import Dataset
-except ImportError:
-    print('Library "torch" not installed. Failed to import.')
-    Dataset = dict
-    torch = namedtuple('torch', 'no_grad')(lambda: lambda fn: fn)  # type: ignore
-
-try:
-    from pandas import DataFrame
-except ImportError:
-    print('Library "pandas" not installed. Failed to import.')
-    DataFrame = dict  # type: ignore
+from datasets import load_dataset
+import pandas as pd
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader, Dataset
+from pandas import DataFrame
 
 from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.metrics import Metrics
@@ -26,6 +18,8 @@ from core_utils.llm.raw_data_importer import AbstractRawDataImporter
 from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor
 from core_utils.llm.task_evaluator import AbstractTaskEvaluator
 from core_utils.llm.time_decorator import report_time
+from torchinfo import summary
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 
 class RawDataImporter(AbstractRawDataImporter):
@@ -41,7 +35,9 @@ class RawDataImporter(AbstractRawDataImporter):
         Raises:
             TypeError: In case of downloaded dataset is not pd.DataFrame
         """
-
+        self._raw_data = load_dataset(self._hf_name, split='test').to_pandas()
+        if type(self._raw_data) is not DataFrame:
+            raise TypeError
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
     """
@@ -55,12 +51,32 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         Returns:
             dict: Dataset key properties
         """
+        dataset_analysis = {
+            'dataset_number_of_samples': len(self._raw_data),
+            'dataset_columns': len(self._raw_data.columns),
+            'dataset_duplicates': len(self._raw_data[self._raw_data.duplicated()]),
+            'dataset_empty_rows': len(self._raw_data[self._raw_data.isna().any(axis=1)])}
+
+        self._raw_data = self._raw_data.dropna()
+
+        dataset_analysis['dataset_sample_min_len'] = len(min(self._raw_data['report'], key=len))
+        dataset_analysis['dataset_sample_max_len'] = len(max(self._raw_data['report'], key=len))
+
+        return dataset_analysis
+
+
 
     @report_time
     def transform(self) -> None:
         """
         Apply preprocessing transformations to the raw dataset.
         """
+        self._data = self._raw_data.rename(columns={'report': 'source', 'summary': 'target'}).\
+            drop_duplicates().reset_index(drop=True)
+
+    @property
+    def data(self):
+        return self._data
 
 
 class TaskDataset(Dataset):
@@ -75,6 +91,7 @@ class TaskDataset(Dataset):
         Args:
             data (pandas.DataFrame): Original data
         """
+        self._data = data
 
     def __len__(self) -> int:
         """
@@ -83,6 +100,7 @@ class TaskDataset(Dataset):
         Returns:
             int: The number of items in the dataset
         """
+        return len(self._data)
 
     def __getitem__(self, index: int) -> tuple[str, ...]:
         """
@@ -94,6 +112,7 @@ class TaskDataset(Dataset):
         Returns:
             tuple[str, ...]: The item to be received
         """
+        return tuple(self._data.iloc[index]['source'],)
 
     @property
     def data(self) -> DataFrame:
@@ -103,6 +122,7 @@ class TaskDataset(Dataset):
         Returns:
             pandas.DataFrame: Preprocessed DataFrame
         """
+        return self._data
 
 
 class LLMPipeline(AbstractLLMPipeline):
@@ -128,6 +148,10 @@ class LLMPipeline(AbstractLLMPipeline):
             batch_size (int): The size of the batch inside DataLoader
             device (str): The device for inference
         """
+        super().__init__(model_name, dataset, max_length, batch_size, device)
+        self._dataset = dataset
+        self._model = AutoModelForSeq2SeqLM.from_pretrained(self._model_name)
+        self._tokenizer = AutoTokenizer.from_pretrained(self._model_name)
 
     def analyze_model(self) -> dict:
         """
@@ -136,6 +160,28 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             dict: Properties of a model
         """
+
+        tensor_data = torch.ones(1,
+                                 self._model.config.decoder.max_position_embeddings,
+                                 dtype=torch.long)
+        input_data = {"input_ids": tensor_data,
+                      "token_type_ids": tensor_data,
+                      "attention_mask": tensor_data}
+
+        model_statistics = summary(model=self._model,
+                                   input_data=input_data,
+                                   decoder_input_ids=tensor_data,
+                                   verbose=False)
+        model_info = {
+            "input_shape": list(input_data['input_ids'].shape),
+            "embedding_size": self._model.config.decoder.max_position_embeddings,
+            "output_shape": model_statistics.summary_list[-1].output_size,
+            "num_trainable_params": model_statistics.trainable_params,
+            "vocab_size": self._model.config.decoder.vocab_size,
+            "size": model_statistics.total_param_bytes,
+            "max_context_length": self._model.config.max_length
+        }
+        return model_info
 
     @report_time
     def infer_sample(self, sample: tuple[str, ...]) -> str | None:
@@ -149,6 +195,10 @@ class LLMPipeline(AbstractLLMPipeline):
             str | None: A prediction
         """
 
+        if not self._model:
+            return None
+        return self._infer_batch((sample,))[0]
+
     @report_time
     def infer_dataset(self) -> DataFrame:
         """
@@ -157,6 +207,14 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             pd.DataFrame: Data with predictions
         """
+        dataset_loader = DataLoader(self._dataset, self._batch_size)
+
+        predictions = []
+
+        for batch in dataset_loader:
+            predictions.extend(self._infer_batch(batch))
+
+        return pd.DataFrame({"target": self._dataset.data['target'], "predictions": predictions})
 
     @torch.no_grad()
     def _infer_batch(self, sample_batch: Sequence[tuple[str, ...]]) -> list[str]:
@@ -169,6 +227,16 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             list[str]: Model predictions as strings
         """
+        tokenizer = AutoTokenizer.from_pretrained(self._model_name)
+        predictions = []
+
+        tokens = tokenizer(sample_batch[0], max_length=120, padding=True,
+                           return_tensors='pt', truncation=True)
+        output = self._model.generate(**tokens)
+        result = tokenizer.batch_decode(output, skip_special_tokens=True)
+        predictions.extend(result)
+
+        return predictions
 
 
 class TaskEvaluator(AbstractTaskEvaluator):
--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -3,14 +3,37 @@ Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
 from core_utils.llm.time_decorator import report_time
-
+from config.constants import PROJECT_ROOT
+from lab_7_llm.main import RawDataImporter, RawDataPreprocessor, TaskDataset, LLMPipeline
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+import json
 
 @report_time
 def main() -> None:
     """
     Run the translation pipeline.
     """
-    result = None
+    with open(PROJECT_ROOT / 'lab_7_llm' / 'settings.json', 'r', encoding='utf-8') as settings:
+        settings = json.load(settings)
+    govreport = RawDataImporter('ccdv/govreport-summarization')
+    govreport.obtain()
+    preprocessor = RawDataPreprocessor(govreport.raw_data)
+    preprocessor.transform()
+    result = preprocessor.analyze()
+    dataset = TaskDataset(preprocessor._data.head(100))
+    pipeline = LLMPipeline(settings['parameters']['model'],
+                           dataset,
+                           max_length=120,
+                           batch_size=5,
+                           device='cpu')
+
+    pipeline.analyze_model()
+    pipeline.infer_sample(dataset[0])
+
+    """dataset_loader = DataLoader(dataset, batch_size=4)
+    pipeline = LLMPipeline(settings['parameters']['model'], dataset, 100, 100, 'cpu')
+    pipeline.analyze_model()"""
     assert result is not None, "Demo does not work correctly"
 
 
