534f99e1d2aa0d9bf81331d1b471623098916d8d
awakenedlilac <90451983+awakenedlilac@users.noreply.github.com>
2024-02-11 10:12:43 +0300

    changed imports (second try)

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -4,13 +4,24 @@ Neural machine translation module.
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 
 from pathlib import Path
-from typing import Iterable, Sequence
-
-import torch
-import pandas as pd
+from typing import Iterable, Iterator, Sequence
 
 from datasets import load_dataset
-from pandas import DataFrame
+
+try:
+    import torch
+    from torch.utils.data.dataset import Dataset
+except ImportError:
+    print('Library "torch" not installed. Failed to import.')
+    Dataset = dict
+    torch = namedtuple('torch', 'no_grad')(lambda: lambda fn: fn)  # type: ignore
+try:
+    from pandas import DataFrame
+except ImportError:
+    print('Library "pandas" not installed. Failed to import.')
+    DataFrame = dict  # type: ignore
+
+from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 from torchinfo import summary
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
