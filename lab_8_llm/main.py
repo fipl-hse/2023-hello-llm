234a3facfe0587917aae9e1cf987916286bff2b7
eskondrashova <katerina_kon02@mail.ru>
2024-02-21 08:45:49 +0300

    mark4

--- lab_8_llm/main.py
+++ lab_8_llm/main.py
@@ -5,6 +5,7 @@ Working with Large Language Models.
 """
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called, duplicate-code
 from collections import namedtuple
+from datasets import load_dataset
 from pathlib import Path
 from typing import Iterable, Sequence
 
@@ -43,6 +44,13 @@ class RawDataImporter(AbstractRawDataImporter):
         Raises:
             TypeError: In case of downloaded dataset is not pd.DataFrame
         """
+        self._raw_data = load_dataset(
+            path=self._hf_name,
+            split="train"
+        ).to_pandas()
+
+        if not isinstance(self._raw_data, DataFrame):
+            raise TypeError("Downloaded dataset is not pd.DataFrame")
 
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
@@ -58,11 +66,31 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
             dict: Dataset key properties
         """
 
+        return {
+            "dataset_number_of_samples": self._raw_data.shape[0],
+            "dataset_columns": self._raw_data.shape[1],
+            "dataset_duplicates": self._raw_data.duplicated().sum(),
+            "dataset_empty_rows": self._raw_data.isnull().any(axis=1).sum(),
+            "dataset_sample_min_len": len(min(self._raw_data["content"], key=len)),
+            "dataset_sample_max_len": len(max(self._raw_data["content"], key=len))
+        }
+
     @report_time
     def transform(self) -> None:
         """
         Apply preprocessing transformations to the raw dataset.
         """
+        self._data = self._raw_data[["content", "grade3"]]
+        self._data = self._data.rename(
+            columns={"grade3": "target",
+                     "content": "source"}
+        )
+        self._data = self._data.dropna()
+        self._data = self._data.reset_index(drop=True)
+        self._data = self._data.replace({"Good": 1,
+                                         "Neutral": 0,
+                                         "Bad": 2})
+        self._data = self._data.reset_index(drop=True)
 
 
 class TaskDataset(Dataset):
@@ -77,6 +105,7 @@ class TaskDataset(Dataset):
         Args:
             data (pandas.DataFrame): Original data
         """
+        self._data = data
 
     def __len__(self) -> int:
         """
@@ -85,6 +114,7 @@ class TaskDataset(Dataset):
         Returns:
             int: The number of items in the dataset
         """
+        return len(self._data)
 
     def __getitem__(self, index: int) -> tuple[str, ...]:
         """
@@ -105,6 +135,7 @@ class TaskDataset(Dataset):
         Returns:
             pandas.DataFrame: Preprocessed DataFrame
         """
+        return self._data
 
 
 class LLMPipeline(AbstractLLMPipeline):
--- lab_8_llm/start.py
+++ lab_8_llm/start.py
@@ -2,15 +2,33 @@
 Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
+from pathlib import Path
+
+from config.lab_settings import LabSettings
 from core_utils.llm.time_decorator import report_time
+from lab_8_llm.main import RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
 @report_time
 def main() -> None:
     """
-    Run the translation pipeline.
+    Run the summarization pipeline.
     """
-    result = None
+
+    root_path = Path(__file__).parent
+    settings = LabSettings(root_path / "settings.json")
+
+    # mark 4
+    importer = RawDataImporter(settings.parameters.dataset)
+    importer.obtain()
+
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+    preprocessor.analyze()
+    preprocessor.transform()
+
+    dataset = TaskDataset(preprocessor.data.head(100))
+
+    result = dataset
     assert result is not None, "Demo does not work correctly"
 
 
