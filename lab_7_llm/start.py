be6293ca4838072e76062622496ce89bc06a6a47
tanynova69 <90447391+tanynova99@users.noreply.github.com>
2024-03-05 15:11:30 +0300

    revert to base before lab 8 pr

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -2,14 +2,7 @@
 Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
-import json
-from pathlib import Path
-
-from config.constants import PROJECT_ROOT
-from core_utils.llm.metrics import Metrics
 from core_utils.llm.time_decorator import report_time
-from lab_7_llm.main import (LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset,
-                            TaskEvaluator)
 
 
 @report_time
@@ -18,41 +11,6 @@ def main() -> None:
     Run the translation pipeline.
     """
     result = None
-
-    with open(PROJECT_ROOT / "lab_7_llm" / "settings.json", "r", encoding="utf-8") as settings_json:
-        settings = json.load(settings_json)
-
-    raw_data = RawDataImporter(settings["parameters"]["dataset"])
-    raw_data.obtain()
-
-    preprocessed_data = RawDataPreprocessor(raw_data.raw_data)
-    print(preprocessed_data.analyze())
-
-    preprocessed_data.transform()
-
-    dataset = TaskDataset(preprocessed_data.data.head(100))
-
-    llm = LLMPipeline(settings["parameters"]["model"], dataset, 120, 2, "cpu")
-
-    print(llm.analyze_model())
-
-    sample_infer = llm.infer_sample(dataset[0])
-    print('prediction for sample (', dataset[0], ')', sample_infer)
-
-    predictions = llm.infer_dataset()
-    predictions_path = PROJECT_ROOT / 'lab_7_llm' / 'dist' / 'predictions.csv'
-
-    if not predictions_path.parent.exists():
-        predictions_path.parent.mkdir()
-    predictions.to_csv(predictions_path, index_label='id')
-
-    evaluator = TaskEvaluator(
-        Path(predictions_path),
-        [Metrics[metric.upper()] for metric in settings['parameters']['metrics']]
-    )
-    result = evaluator.run()
-    print(result)
-
     assert result is not None, "Demo does not work correctly"
 
 
