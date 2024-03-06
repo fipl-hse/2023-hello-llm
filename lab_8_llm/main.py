e5b563ad0e93ce2ecfbff25f21000f9acc599e9a
tanynova99 <tanynovark800@gmail.com>
2024-03-06 13:02:01 +0300

    IMPORTS-

--- lab_8_llm/main.py
+++ lab_8_llm/main.py
@@ -19,9 +19,10 @@ from transformers import GPTNeoXForCausalLM, AutoTokenizer
 from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.metrics import Metrics
 from core_utils.llm.raw_data_importer import AbstractRawDataImporter
-from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor
+from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor, ColumnNames
 from core_utils.llm.task_evaluator import AbstractTaskEvaluator
 from core_utils.llm.time_decorator import report_time
+
 
 class RawDataImporter(AbstractRawDataImporter):
     """
