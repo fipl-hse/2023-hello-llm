8d6e75b178038a353b2bba8469ec694f63faa7e5
Anastasia Matuschak <anastasia.mt12@gmail.com>
2024-03-06 19:14:50 +0300

    delete lab 7

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -2,11 +2,7 @@
 Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
-import json
-
-from config.constants import PROJECT_ROOT
 from core_utils.llm.time_decorator import report_time
-from lab_7_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
 @report_time
@@ -14,25 +10,7 @@ def main() -> None:
     """
     Run the translation pipeline.
     """
-    with open(PROJECT_ROOT / 'lab_7_llm' / 'settings.json', 'r', encoding='utf-8') as file:
-        settings = json.load(file)
-    data_importer = RawDataImporter(settings['parameters']['dataset'])
-    data_importer.obtain()
-
-    preprocessor = RawDataPreprocessor(data_importer.raw_data)
-    preprocessor.analyze()
-    preprocessor.transform()
-
-    dataset = TaskDataset(preprocessor.data.head(100))
-
-    pipeline = LLMPipeline(settings['parameters']['model'], dataset,
-                           max_length=120, batch_size=1, device='cpu')
-
-    model_analysis = pipeline.analyze_model()
-    print(model_analysis)
-
-    result = pipeline.infer_sample(dataset[0])
-
+    result = None
     assert result is not None, "Demo does not work correctly"
 
 
