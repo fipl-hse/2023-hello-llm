2e2fdeefc911a6d2f0b83166a909a7d7a2bedd22
AnnaSizykh <aasizykh@edu.hse.ru>
2024-03-06 15:35:19 +0300

    attempt to overwrite lab_7 files

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -2,10 +2,7 @@
 Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
-import json
-from config.constants import PROJECT_ROOT
 from core_utils.llm.time_decorator import report_time
-from lab_7_llm.main import RawDataImporter, RawDataPreprocessor, TaskDataset, LLMPipeline
 
 
 @report_time
@@ -13,19 +10,7 @@ def main() -> None:
     """
     Run the translation pipeline.
     """
-
-    with open(PROJECT_ROOT / 'lab_7_llm' / 'settings.json', 'r', encoding='utf-8') as settings:
-        settings_dict = json.load(settings)
-    raw_dataset = RawDataImporter(settings_dict["parameters"]["dataset"])
-    raw_dataset.obtain()
-    processed_dataset = RawDataPreprocessor(raw_dataset.raw_data)
-    analysis = processed_dataset.analyze()
-    #processed_dataset.transform()
-    #dataset = TaskDataset(processed_dataset.data.head(100))
-    #pipeline = LLMPipeline(settings_dict['parameters']['model'], dataset, 512, 64, 'cpu')
-    #pipeline.analyze_model()
-
-    result = 1
+    result = None
     assert result is not None, "Demo does not work correctly"
 
 
