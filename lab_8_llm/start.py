cd7ed2580c2c58fb2eb732906f6fd260b9b08ba7
tatarinovst2 <SpaceUltraMax@yandex.ru>
2024-03-08 05:35:24 +0300

    90% copied code from lab 7. Will probably work

--- lab_8_llm/start.py
+++ lab_8_llm/start.py
@@ -1,8 +1,13 @@
 """
 Neural machine translation starter.
 """
+from pathlib import Path
+
+from config.lab_settings import LabSettings
 # pylint: disable= too-many-locals
 from core_utils.llm.time_decorator import report_time
+from lab_8_llm.main import (LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset,
+                            TaskEvaluator)
 
 
 @report_time
@@ -10,7 +15,48 @@ def main() -> None:
     """
     Run the translation pipeline.
     """
-    result = None
+    root_path = Path(__file__).parent
+    settings = LabSettings(root_path / "settings.json")
+    predictions_path = root_path / "dist" / "predictions.csv"
+
+    if not predictions_path.parent.exists():
+        predictions_path.parent.mkdir(exist_ok=True)
+
+    device = "mps"
+    batch_size = 1
+    max_length = 120
+    num_samples = 10
+
+    raw_data_importer = RawDataImporter(settings.parameters.dataset)
+    raw_data_importer.obtain()
+
+    if raw_data_importer.raw_data is None:
+        raise ValueError("raw_data_importer.raw_data is None.")
+
+    raw_data_preprocessor = RawDataPreprocessor(raw_data_importer.raw_data)
+    print(raw_data_preprocessor.analyze())
+
+    # Mark 6
+    raw_data_preprocessor.transform()
+    dataset = TaskDataset(raw_data_preprocessor.data.head(num_samples))
+    pipeline = LLMPipeline(settings.parameters.model, dataset, max_length, batch_size, device)
+
+    analysis = pipeline.analyze_model()
+    print(analysis)
+
+    prediction = pipeline.infer_sample(next(iter(dataset)))
+    print(prediction)
+
+    # Mark 8
+    batch_size = 64
+    pipeline = LLMPipeline(settings.parameters.model, dataset, max_length, batch_size, device)
+    predictions_df = pipeline.infer_dataset()
+    predictions_df.to_csv(predictions_path, index=False, encoding='utf-8')
+
+    evaluator = TaskEvaluator(predictions_path, settings.parameters.metrics)
+    result = evaluator.run()
+    print(result)
+
     assert result is not None, "Demo does not work correctly"
 
 
