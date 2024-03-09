7aed294e02ade38188da3d397df74a67a091a7b6
DashaKrinochkina <dasha060402@mail.ru>
2024-03-09 12:44:40 +0300

    delete lab 7

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -6,8 +6,6 @@ from collections import namedtuple
 from pathlib import Path
 from typing import Iterable, Sequence
 
-from datasets import load_dataset
-
 try:
     import torch
     from torch.utils.data.dataset import Dataset
@@ -22,14 +20,10 @@ except ImportError:
     print('Library "pandas" not installed. Failed to import.')
     DataFrame = dict  # type: ignore
 
-from torch.utils.data.dataset import Dataset
-from torchinfo import summary
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
 from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.metrics import Metrics
 from core_utils.llm.raw_data_importer import AbstractRawDataImporter
-from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor, ColumnNames
+from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor
 from core_utils.llm.task_evaluator import AbstractTaskEvaluator
 from core_utils.llm.time_decorator import report_time
 
@@ -47,8 +41,6 @@ class RawDataImporter(AbstractRawDataImporter):
         Raises:
             TypeError: In case of downloaded dataset is not pd.DataFrame
         """
-        dataset = load_dataset(self._hf_name, name="1.0.0", split="test")
-        self._raw_data = dataset.to_pandas()
 
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
@@ -63,27 +55,12 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         Returns:
             dict: Dataset key properties
         """
-        analyzed = {'dataset_number_of_samples': len(self._raw_data),
-                    'dataset_columns': len(self._raw_data.columns),
-                    'dataset_duplicates': self._raw_data.duplicated().sum(),
-                    'dataset_empty_rows': self._raw_data.isna().sum().sum(),
-                    'dataset_sample_min_len': len(min(self._raw_data['article'], key=len)),
-                    'dataset_sample_max_len': len(max(self._raw_data['article'], key=len))}
-
-        return analyzed
 
     @report_time
     def transform(self) -> None:
         """
         Apply preprocessing transformations to the raw dataset.
         """
-        self._data = (self._raw_data.drop(columns='id')
-                      .rename(columns={'article': ColumnNames.SOURCE.value,
-                                       'highlights': ColumnNames.TARGET.value})
-                      .drop_duplicates()
-                      .reset_index(drop=True))
-
-        self._data['source'] = self._data['source'].str.replace('(CNN)', '')
 
 
 class TaskDataset(Dataset):
@@ -98,7 +75,6 @@ class TaskDataset(Dataset):
         Args:
             data (pandas.DataFrame): Original data
         """
-        self._data = data
 
     def __len__(self) -> int:
         """
@@ -107,7 +83,6 @@ class TaskDataset(Dataset):
         Returns:
             int: The number of items in the dataset
         """
-        return len(self._data)
 
     def __getitem__(self, index: int) -> tuple[str, ...]:
         """
@@ -119,7 +94,6 @@ class TaskDataset(Dataset):
         Returns:
             tuple[str, ...]: The item to be received
         """
-        return self._data["source"].iloc[index]
 
     @property
     def data(self) -> DataFrame:
@@ -129,7 +103,6 @@ class TaskDataset(Dataset):
         Returns:
             pandas.DataFrame: Preprocessed DataFrame
         """
-        return self._data
 
 
 class LLMPipeline(AbstractLLMPipeline):
@@ -137,12 +110,14 @@ class LLMPipeline(AbstractLLMPipeline):
     A class that initializes a model, analyzes its properties and infers it.
     """
 
-    def __init__(self,
-                 model_name: str,
-                 dataset: TaskDataset,
-                 max_length: int,
-                 batch_size: int,
-                 device: str) -> None:
+    def __init__(
+            self,
+            model_name: str,
+            dataset: TaskDataset,
+            max_length: int,
+            batch_size: int,
+            device: str
+    ) -> None:
         """
         Initialize an instance of LLMPipeline.
 
@@ -153,9 +128,6 @@ class LLMPipeline(AbstractLLMPipeline):
             batch_size (int): The size of the batch inside DataLoader
             device (str): The device for inference
         """
-        super().__init__(model_name, dataset, max_length, batch_size, device)
-        self._model = AutoModelForSeq2SeqLM.from_pretrained(self._model_name)
-        self._tokenizer = AutoTokenizer.from_pretrained(self._model_name)
 
     def analyze_model(self) -> dict:
         """
@@ -164,29 +136,6 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             dict: Properties of a model
         """
-        embeddings_length = self._model.config.decoder.max_position_embeddings
-        tensor = torch.ones(1, embeddings_length, dtype=torch.long)
-
-        ids = {"input_ids": tensor,
-               "token_type_ids": tensor,
-               "attention_mask": tensor}
-
-        statistics = summary(self._model,
-                             input_data=ids,
-                             decoder_input_ids=tensor,
-                             verbose=False)
-
-        model_info = {
-            "input_shape": list(statistics.input_size['input_ids']),
-            "embedding_size": embeddings_length,
-            "output_shape": statistics.summary_list[-1].output_size,
-            "num_trainable_params": statistics.trainable_params,
-            "vocab_size": self._model.config.decoder.vocab_size,
-            "size": statistics.total_param_bytes,
-            "max_context_length": self._model.config.max_length
-        }
-
-        return model_info
 
     @report_time
     def infer_sample(self, sample: tuple[str, ...]) -> str | None:
@@ -199,15 +148,6 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             str | None: A prediction
         """
-        tokens = self._tokenizer(sample[0], max_length=120, padding=True,
-                                 return_tensors='pt', truncation=True)
-        print(tokens.keys())
-
-        output = self._model.generate(**tokens)
-        result = self._tokenizer.batch_decode(output, skip_special_tokens=True)
-
-        return result[0]
-
 
     @report_time
     def infer_dataset(self) -> DataFrame:
