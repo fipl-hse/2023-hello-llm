425aadf96525a346eb2080103f96f3b9b410de39
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2024-03-09 16:40:12 +0300

    upd

--- lab_8_llm/assets/main.js
+++ lab_8_llm/assets/main.js
@@ -5,13 +5,12 @@ let sample_inference = async (question, result) => {
                 "Content-type": "application/json"
             },
             body: JSON.stringify({
-                question: question
+                question: question.value
             })
         })
         let results_infer = await response.json();
         console.log(results_infer['infer'])
         result.innerHTML = results_infer['infer'];
-        // result.appendChild(document.createTextNode(results_infer['infer'.toUpperCase()]))
 }
 
 window.onload = function(){
