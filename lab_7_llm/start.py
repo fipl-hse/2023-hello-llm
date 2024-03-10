ed83dec3829ee7c735677c7240cb2adcb204e556
eapareshina <zhenya.pareshina@gmail.com>
2024-03-10 21:26:54 +0300

    style import fixes

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -2,10 +2,10 @@
 Neural machine translation starter.
 """
 # pylint: disable= too-many-locals
-from core_utils.llm.time_decorator import report_time
 import json
 
 from config.constants import PROJECT_ROOT
+from core_utils.llm.time_decorator import report_time
 from lab_7_llm.main import (
     LLMPipeline,
     RawDataImporter,
