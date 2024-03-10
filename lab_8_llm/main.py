83cc18f50557b1440848723bb00239237794d1fc
Irinae <ikurnakova2002@mail.ru>
2024-03-10 15:11:14 +0300

    changes

--- lab_8_llm/main.py
+++ lab_8_llm/main.py
@@ -29,7 +29,6 @@ class RawDataImporter(AbstractRawDataImporter):
     """
     A class that imports the HuggingFace dataset.
     """
-    _raw_data: DataFrame
 
     @report_time
     def obtain(self) -> None:
@@ -41,12 +40,8 @@ class RawDataImporter(AbstractRawDataImporter):
         """
         self._raw_data = load_dataset(self._hf_name, split='test').to_pandas()
 
-        if not isinstance(self._raw_data, DataFrame):
+        if not isinstance(self._raw_data, pd.DataFrame):
             raise TypeError
-
-    @property
-    def raw_data(self) -> DataFrame:
-        return self._raw_data
 
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
