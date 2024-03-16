2decfcea1794bb404c7749d9db7da827f0defcab
Smirnova23 <aasmirnova_23@edu.hse.ru>
2024-03-17 00:19:28 +0300

    fixing imports and other changes

--- lab_8_llm/start.py
+++ lab_8_llm/start.py
@@ -6,7 +6,7 @@ import json
 
 from config.constants import PROJECT_ROOT
 from core_utils.llm.time_decorator import report_time
-from lab_8_llm.main import (LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset)
+from lab_8_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
 @report_time
