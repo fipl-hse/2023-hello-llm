0e1f0826c1fe604c4e3049ba5b1b0ef23375ac43
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-03-16 18:09:46 +0300

    Simplify imports

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -2,31 +2,17 @@
 Neural machine translation module.
 """
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
-from collections import namedtuple
 from pathlib import Path
 from typing import Iterable, Sequence
 
+import numpy as np
+import pandas as pd
+import torch
+from datasets import load_dataset
 from evaluate import load
 from torch.utils.data import DataLoader
 from torchinfo import torchinfo
 from transformers import AlbertForSequenceClassification, AutoTokenizer
-
-try:
-    import torch
-    from torch.utils.data.dataset import Dataset
-except ImportError:
-    print('Library "torch" not installed. Failed to import.')
-    Dataset = dict
-    torch = namedtuple('torch', 'no_grad')(
-        lambda: lambda fn: fn)  # type: ignore
-
-try:
-    from pandas import DataFrame, read_csv
-except ImportError:
-    print('Library "pandas" not installed. Failed to import.')
-    DataFrame = dict  # type: ignore
-
-from datasets import load_dataset
 
 from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.metrics import Metrics
