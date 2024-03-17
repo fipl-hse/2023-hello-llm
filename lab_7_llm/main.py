6a3983af754e2e4fe7b262a5fba2e231bc28f61e
Burakovan <burakova.02@mailru>
2024-03-17 20:21:50 +0300

    changed dataset

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -46,7 +46,7 @@ class RawDataImporter(AbstractRawDataImporter):
             TypeError: In case of downloaded dataset is not pd.DataFrame
         """
         self._raw_data = load_dataset(path=self._hf_name,
-                                      name='document', split='test').to_pandas()
+                                      name='section', split='test').to_pandas()
 
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
